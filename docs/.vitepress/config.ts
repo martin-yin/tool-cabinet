72acc3d9f3c57d76a1d2d29f18c9cfa15c00e7e0
martin-yin <martin-yin@foxmail.com>
2022-04-20 22:04:22 +0800

    fix: 修复css引用问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,26 +1,26 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: "Web Dev Tools",
+  title: 'Web Dev Tools',
   lastUpdated: true,
-
+  base: 'https://martin-yin.github.io/web-dev-tools/',
   themeConfig: {
-    repo: "martin-yin/web-dev-tools",
-    docsDir: "docs",
-    docsBranch: "main",
-    lastUpdated: " 最后一次更新",
-    nav: [{ text: "介绍", link: "/" }],
+    repo: 'martin-yin/web-dev-tools',
+    docsDir: 'docs',
+    docsBranch: 'main',
+    lastUpdated: ' 最后一次更新',
+    nav: [{ text: '介绍', link: '/' }],
     sidebar: {
-      "/": getGuideSidebar(),
-    },
-  },
-});
+      '/': getGuideSidebar()
+    }
+  }
+})
 
 function getGuideSidebar() {
   return [
     {
-      text: "介绍",
-      children: [{ text: "Web Dev Tools", link: "/" }],
-    },
-  ];
+      text: '介绍',
+      children: [{ text: 'Web Dev Tools', link: '/' }]
+    }
+  ]
 }
