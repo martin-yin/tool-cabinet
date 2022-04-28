1290e393baeafa582c25ad3009832e59449e0628
martin-yin <martin-yin@foxmail.com>
2022-04-28 14:16:30 +0800

    test: 新增file.ts 的测试

---
+++ packages/code-generate/__test__/utils/file.test.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import { dotExistDirectoryCreate, generateFile } from '../../src/utils/file'
+import fs from 'fs'
+
+jest.mock('chalk', () => ({
+  green: jest.fn(),
+  blue: jest.fn(),
+  red: jest.fn()
+}))
+
+describe('file.test.ts', () => {
+  describe('Test dotExistDirectoryCreate', () => {
+    test('empty dir', async () => {
+      const result = await dotExistDirectoryCreate('')
+      expect(result).toBe(false)
+    })
+
+    test('real dir', async () => {
+      const result = await dotExistDirectoryCreate('C:/Users/martin-yin/Desktop')
+      expect(result).toBe(true)
+    })
+  })
+
+  describe('Test generateFile', () => {
+    const basePath = path.resolve()
+    const filePath = `${basePath}/testGenerateFile.txt`
+
+    afterAll(() => {
+      fs.unlink(filePath, err => {
+        if (err) throw err
+      })
+    })
+
+    test('generateFile success', async () => {
+      const result = await generateFile(basePath, 'testGenerateFile.txt', 'just test write file')
+      expect(result).toBe(true)
+      const fileContent = fs.readFileSync(filePath, 'utf-8')
+      expect(fileContent).toBe('just test write file')
+    })
+
+    test('generateFile fail', async () => {
+      const result = await generateFile(basePath, 'testGenerateFile.txt', 'just test write file')
+      expect(result).toBe(false)
+    })
+  })
+})
--- packages/code-generate/src/utils/file.ts
+++ packages/code-generate/src/utils/file.ts
@@ -1,46 +1,35 @@
 import fs from 'fs'
 import path from 'path'
 import chalk from 'chalk'
+import { isEmpty } from 'underscore'
 
-export function dotExistDirectoryCreate(directory: string) {
-  return new Promise(resolve => {
-    mkdirs(directory, function () {
-      resolve(true)
-    })
-  })
-}
-
-export function mkdirs(directory: string, callback: () => void) {
-  var exists = fs.existsSync(directory)
-  if (exists) {
-    callback()
-  } else {
-    mkdirs(path.dirname(directory), function () {
-      fs.mkdirSync(directory)
-      callback()
-    })
+export async function dotExistDirectoryCreate(directory: string): Promise<boolean> {
+  const { dir } = path.parse(directory)
+  if (isEmpty(dir)) {
+    console.log('路径不能为空')
+    return false
   }
+  if (fs.existsSync(directory)) {
+    return true
+  }
+  fs.mkdirSync(directory, { recursive: true })
+  return true
 }
 
 export async function generateFile(directory: string, file: string, data: string): Promise<boolean> {
-  if (!fs.existsSync(directory)) {
-    await dotExistDirectoryCreate(directory)
-  }
   if (fs.existsSync(`${directory}/${file}`)) {
     console.log(chalk.red(`创建文件失败，${file}文件已经存在 \n`))
     return false
   }
-
-  return new Promise((resolve, reject) => {
-    fs.writeFile(`${directory}/${file}`, data, 'utf8', err => {
-      console.log(chalk.blue(`正在创建创建文件: ${directory}${file} \n`))
-      if (err) {
-        console.log(chalk.red(`创建文件失败，${file}文件已经存在 \n`))
-        reject(err)
-      } else {
-        console.log(chalk.green(`创建创建文件成功: ${file} \n`))
-        resolve(true)
-      }
-    })
-  })
+  if (await dotExistDirectoryCreate(directory)) {
+    console.log(chalk.blue(`正在创建创建文件: ${directory}${file} \n`))
+    try {
+      fs.writeFileSync(`${directory}/${file}`, data, 'utf8')
+      console.log(chalk.green(`创建创建文件成功: ${file} \n`))
+      return true
+    } catch (error) {
+      console.log(chalk.red(`创建文件失败, ${error.message} \n`))
+      return false
+    }
+  }
 }
