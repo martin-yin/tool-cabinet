3a44111dc9fab5fe45fbee0390d8527c5f18d0dd
martin-yin <martin-yin@foxmail.com>
2022-05-12 13:25:51 +0800

    test: test 完善

--- packages/generate-request-code/__test__/utils/repositoryRequest.test.ts
+++ packages/generate-request-code/__test__/utils/repositoryRequest.test.ts
@@ -2,21 +2,23 @@ import { RepositoryRequest } from '../../src/utils/request'
 
 describe('repositoryRequest.test', () => {
   test('requestConfig with empty object ', async () => {
-    RepositoryRequest.initService(null)
-    const result = await RepositoryRequest.request({
+    const service = new RepositoryRequest({
+      requestConfig: null
+    })
+    const result = await service.request({
       url: 'http://127.0.0.1:3000/dictionary/10'
     })
     expect(result.data.code).toBe(0)
   })
 
   test('requestAfter with function', async () => {
-    RepositoryRequest.initService(
-      {
+    const service = new RepositoryRequest({
+      requestConfig: {
         baseURL: 'http://127.0.0.1:3000',
         timeout: 60000
       },
-      null,
-      (response: any) => {
+      interceptorRequest: null,
+      interceptorResponse: (response: any) => {
         const responseCode = response.status
         if (responseCode === 200) {
           return Promise.resolve(response.data.data)
@@ -24,8 +26,8 @@ describe('repositoryRequest.test', () => {
           return Promise.reject(response)
         }
       }
-    )
-    const result: any = await RepositoryRequest.request({
+    })
+    const result: any = await service.request({
       url: '/dictionary/10'
     })
     expect(result.id).toBe(10)
